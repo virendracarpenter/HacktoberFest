ebad6a4e0240ff03098bc663dca276bdf807e4e1
Bhupendra Choudhary <80590016+Bhupen15@users.noreply.github.com>
2021-10-19 14:17:07 +0530

    "for-loop-concept"
    In python range function never print last value where the range end.

--- cs/python/forloopconcept.py
+++ cs/python/forloopconcept.py
@@ -2,5 +2,5 @@ for i in [1,2,3,4]: #range(4)
     print("Hello " + str(i))
 
 
-for i in range(4,15):
+for i in range(4,15): #in python range function never print the last value 
     print("Hello " + str(i))
