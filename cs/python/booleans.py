eab00b172582855f37a779675ed7a1b5f34e8014
Prashant Aditya <77530131+Darkcoder-paditya@users.noreply.github.com>
2022-10-04 22:50:43 +0530

    Proposed some python files
    Proposed some python files
    I am a beginner for open-source, hence starting my journey by contributing for hacktoberfest 2022. 
    Please accept my pull request.
    Name:- Prashant Aditya
    ID- Darkcoder-paditya

---
+++ cs/python/booleans.py
@@ -0,0 +1,18 @@
+# Boolean value
+val1 = True
+print(val1)
+
+# Number to Boolean
+number = 10
+print(bool(number))
+
+number = -5
+print(bool(number))
+
+number = 0
+print(bool(number))
+
+# Boolean from comparison operator
+val1 = 6
+val2 = 3
+print(val1 < val2)
---
+++ cs/python/foloop.py
@@ -0,0 +1,6 @@
+# Initialize the list
+weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"]
+print("Seven Weekdays are:\n")
+# Iterate the list using for loop
+for day in range(len(weekdays)):
+    print(weekdays[day])
